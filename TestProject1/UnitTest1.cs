64c59841303e543ca7510f21852053a3ba29b758
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-04-08 21:51:00 +0700

    add test

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using DotNetSonarLens;
+using DotNetSonarLens.Controllers;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            WeatherForecastController controller = new WeatherForecastController();
+            var result = controller.Get();
+            Assert.NotNull(result);
+            Assert.IsType<WeatherForecast[]>(result);
+            Assert.Equal(5, result.Count());
+
+        }
+    }
+}
